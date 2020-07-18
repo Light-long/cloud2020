c9939a89b8601726e73e0dd7d2dbe9012bbcb7a0
Shilong Li <lishilong1108@foxmail.com>
2020-07-18 18:33:23 +0800

    serverPort

--- cloud-provider-payment8001/src/main/java/com/tx/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/tx/springcloud/controller/PaymentController.java
@@ -4,6 +4,7 @@ import com.tx.springcloud.entity.CommonResult;
 import com.tx.springcloud.entity.Payment;
 import com.tx.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,12 +18,15 @@ public class PaymentController {
     @Resource
     private PaymentService paymentService;
 
+    @Value("${server.port}")
+    private String serverPort;
+
     @PostMapping(value = "/create")
     public CommonResult create(@RequestBody Payment payment) {
         int result = paymentService.create(payment);
         log.info("result="+result);
         if (result > 0) {
-            return new CommonResult(200,"插入数据库成功",result);
+            return new CommonResult(200,"插入数据库成功,serverPort: "+serverPort,result);
         } else {
             return new CommonResult(404,"插入数据库失败",null);
         }
@@ -33,7 +37,7 @@ public class PaymentController {
         Payment payment = paymentService.getPaymentById(id);
         log.info("payment="+payment);
         if (!StringUtils.isEmpty(payment)) {
-            return new CommonResult(200,"查询成功",payment);
+            return new CommonResult(200,"查询成功,serverPort: "+serverPort,payment);
         } else {
             return new CommonResult(404,"没有对应记录"+id,null);
         }
