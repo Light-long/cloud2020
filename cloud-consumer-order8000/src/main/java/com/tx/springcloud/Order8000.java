bca06c7254c6635e47056199db8b92eceb5ee825
Shilong Li <lishilong1108@foxmail.com>
2020-07-18 09:33:28 +0800

    Order8000

---
+++ cloud-consumer-order8000/src/main/java/com/tx/springcloud/Order8000.java
@@ -0,0 +1,12 @@
+package com.tx.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Order8000 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Order8000.class,args);
+    }
+}
---
+++ cloud-consumer-order8000/src/main/java/com/tx/springcloud/controller/OrderController.java
@@ -0,0 +1,36 @@
+package com.tx.springcloud.controller;
+
+import com.tx.springcloud.entity.CommonResult;
+import com.tx.springcloud.entity.Payment;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+
+@RestController
+@Slf4j
+@RequestMapping(value = "/consumer")
+public class OrderController {
+
+    private static final String PAYMENT_URL = "http://localhost:8001";
+    private static final String PAYMENT_CREATE = "/payment/create";
+    private static final String PAYMENT_GET_PAYMENT = "/payment/get/";
+
+    @Resource
+    private RestTemplate restTemplate;
+
+    @GetMapping("/payment/create")
+    public CommonResult<Payment> create(Payment payment) {
+        return restTemplate.postForObject(PAYMENT_URL+PAYMENT_CREATE,payment,CommonResult.class);
+    }
+
+    @GetMapping("/payment/get/{id}")
+    public CommonResult<Payment> getPayment(@PathVariable("id") Long id) {
+        return restTemplate.getForObject(PAYMENT_URL+PAYMENT_GET_PAYMENT+id,CommonResult.class);
+    }
+
+}
