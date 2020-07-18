ad154d239c77455522c9a58b397313d4b478cbc4
Shilong Li <lishilong1108@foxmail.com>
2020-07-18 10:55:51 +0800

    配置eureka-client

--- cloud-provider-payment8001/src/main/java/com/tx/springcloud/Payment8001.java
+++ cloud-provider-payment8001/src/main/java/com/tx/springcloud/Payment8001.java
@@ -2,8 +2,10 @@ package com.tx.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class Payment8001 {
     public static void main(String[] args) {
         SpringApplication.run(Payment8001.class,args);
