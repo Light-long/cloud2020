4c5f33b910607db620838e7fe0dd593329cd8d03
Shilong Li <lishilong1108@foxmail.com>
2020-07-19 10:53:08 +0800

    SpringCloud整合Zookeeper

---
+++ cloud-consumerzk-order8000/src/main/java/com/tx/springcloud/controller/OrderZKController.java
@@ -0,0 +1,26 @@
+package com.tx.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+
+@RestController
+@Slf4j
+@RequestMapping("/consumer")
+public class OrderZKController {
+
+    private static final String INVOKE_URL = "http://cloud-provider-payment";
+
+    @Resource
+    private RestTemplate restTemplate;
+
+    @GetMapping("/payment/zk")
+    public String paymentInfo() {
+        String result = restTemplate.getForObject(INVOKE_URL + "/payment/zk", String.class);
+        return result;
+    }
+}
